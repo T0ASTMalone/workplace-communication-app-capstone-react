864a673e3c3b0400622733d6345518f3a40755ca
Miguel Ponce <miguelaponce258@gmail.com>
2019-12-08 14:53:47 -0700

    implemented controlled inputs

--- src/components/Ideas/IdeasForm.js
+++ src/components/Ideas/IdeasForm.js
@@ -1,11 +1,76 @@
 import React, { Component } from "react";
+import InputError from "./IdeasFormError";
 import "./IdeasForm.css";
 
 export default class IdeasFrom extends Component {
+  state = {
+    title: { value: "", touched: false },
+    idea: { value: "", touched: false }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.validateTitle() || this.validateIdea()) {
+      this.setAllToTouched();
+    } else {
+      const { title, idea } = this.state;
+      let newIdea = {
+        user: "", //get user from context,
+        title: title.value,
+        idea: idea.value,
+        date: new Date()
+      };
+      this.clearValues();
+      console.log(newIdea);
+    }
+  };
+
+  clearValues = () => {
+    this.setState({
+      title: { value: "", touched: false },
+      idea: { value: "", touched: false }
+    });
+  };
+
+  setAllToTouched = () => {
+    const { title, idea } = this.state;
+    this.setState({
+      title: { value: title.value, touched: true },
+      idea: { value: idea.value, touched: true }
+    });
+  };
+
+  updateTitle = title => {
+    this.setState({ title: { value: title, touched: true } });
+  };
+
+  updateIdea = idea => {
+    this.setState({ idea: { value: idea, touched: true } });
+  };
+
+  validateTitle = () => {
+    const title = this.state.title.value;
+    if (title < 1) {
+      return "A user name is required";
+    }
+  };
+
+  validateIdea = () => {
+    const idea = this.state.idea.value;
+    if (idea < 1) {
+      return "A idea is required";
+    }
+  };
+
   render() {
+    const { title, idea } = this.state;
     return (
       <div className="ideas-form-container">
-        <form action="" className="idea-form">
+        <form
+          action=""
+          className="idea-form"
+          onSubmit={e => this.handleSubmit(e)}
+        >
           <legend>
             <h3>Have an Idea?</h3>
           </legend>
@@ -18,12 +83,22 @@ export default class IdeasFrom extends Component {
             id="idea-title"
             className="idea-item"
             placeholder="Title"
+            value={title.value}
+            onChange={e => this.updateTitle(e.target.value)}
           />
+          <InputError hasError={this.validateTitle()} touched={title.touched} />
           {/* idea content */}
           <label htmlFor="idea-content" className="idea-item">
             Idea
           </label>
-          <input type="text" id="idea-content" className="idea-item" />
+          <input
+            type="text"
+            id="idea-content"
+            className="idea-item"
+            value={idea.value}
+            onChange={e => this.updateIdea(e.target.value)}
+          />
+          <InputError hasError={this.validateIdea()} touched={idea.touched} />
           <button className="post-idea idea-item">Send</button>
         </form>
       </div>
