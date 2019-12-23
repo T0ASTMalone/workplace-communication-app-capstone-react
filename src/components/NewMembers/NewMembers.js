bd555d662c1935f9ff037d106ce9f9267d84765e
Miguel Ponce <miguelaponce258@gmail.com>
2019-12-23 01:00:16 -0700

    added error message

--- src/components/NewMembers/NewMembers.js
+++ src/components/NewMembers/NewMembers.js
@@ -7,12 +7,16 @@ export default function NewMembers() {
   // pending users
   let [penUsers, setPenUsers] = useState([]);
 
+  // err
+  let [err, setErr] = useState("");
+
   // show pending users
   let [show, setShow] = useState(false);
 
   const handleAccept = name => {
-    console.log(name);
     // PATCH user type from 'pending' to 'member'
+    let user = testUsers.find(penUser => penUser.nickname === name);
+    user.user_type = "member";
     const remainingUsers = penUsers.filter(user => user.nickname !== name);
     // update pending users in state
     setPenUsers(remainingUsers);
@@ -22,6 +26,9 @@ export default function NewMembers() {
     //fetch pending users
     let users = testUsers.filter(user => user.user_type === "pending");
     // toggle pending users
+    if (users.length < 1) {
+      setErr(`There are no pending users`);
+    }
     if (penUsers.length > 1) {
       setPenUsers([]);
     } else {
@@ -33,6 +40,8 @@ export default function NewMembers() {
 
   const handleDecline = name => {
     // delete users
+    let user = testUsers.find(penUser => penUser.nickname === name);
+    user.user_type = "declined";
     let users = penUsers.filter(user => user.nickname !== name);
     // update users
     setPenUsers(users);
@@ -41,7 +50,7 @@ export default function NewMembers() {
   return (
     <div
       className={
-        show && penUsers.length >= 1
+        show && (penUsers.length >= 1 || err)
           ? "pending-members show-members"
           : "pending-members"
       }
@@ -49,6 +58,7 @@ export default function NewMembers() {
       <button className="pen-title" onClick={handleShowPenUsers}>
         Pending Members
       </button>
+      <p className="err">{err}</p>
       {penUsers.length >= 1 ? (
         penUsers.map((user, i) => (
           <PendingMember
