13a7b808320c6dc173851d0d99879c556efd1cfb
Miguel Ponce <miguelaponce258@gmail.com>
2019-12-28 13:06:57 -0700

    feat: implement get wp posts

--- src/Services/wp-api-service.js
+++ src/Services/wp-api-service.js
@@ -12,6 +12,19 @@ const WpService = {
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
+  },
+
+  getWpPosts(id) {
+    console.log(id);
+    return fetch(`${config.API_ENDPOINT}/posts/${id}/wp`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      }
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
   }
 };
 
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,36 +1,37 @@
 import React, { Component } from "react";
 import PostForm from "../PostForm/PostForm";
 import Post from "../Post/Post";
-import dummyPosts from "../../dummy-posts";
+//import dummyPosts from "../../dummy-posts";
 import WorkPlaceContext from "../../context/WorkPlaceContext";
 import "./Feed.css";
+import WpService from "../../Services/wp-api-service";
 
 export default class Feed extends Component {
   state = {
+    err: null,
     posts: null
   };
 
   static contextType = WorkPlaceContext;
 
   async componentDidMount() {
-    //fetch most recent posts from database where workplace
-    await this.context.setPosts(
-      dummyPosts.filter(post => post.type === "posts")
-    );
+    //fetch posts for workplace
+    await WpService.getWpPosts(this.context.wpId)
+      .then(posts =>
+        //set posts in state
+        this.setState({ posts })
+      )
+      .catch(err => this.setState({ err }));
   }
 
   render() {
-    let posts = this.context.posts;
+    let posts = this.state.posts;
     return (
       <div className="feed">
         <div className="post">
           <PostForm />
         </div>
-        {posts ? (
-          posts.map((post, i) => <Post key={i} post={post} />)
-        ) : (
-          <>Looks like no one has posted anything yet</>
-        )}
+        {posts ? posts.map((post, i) => <Post key={i} post={post} />) : <></>}
       </div>
     );
   }
