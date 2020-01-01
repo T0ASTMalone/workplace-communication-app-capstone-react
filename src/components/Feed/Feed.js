c59c17794f26183ebb8083a21901dbc5f961a6cc
Miguel Ponce <miguelaponce258@gmail.com>
2019-12-31 18:35:41 -0700

    feat: implement pagination

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -7,32 +7,61 @@ import WpService from "../../Services/wp-api-service";
 
 export default class Feed extends Component {
   state = {
-    err: null
+    err: null,
+    offset: 1,
+    disableLoadMore: false
   };
 
   static contextType = WorkPlaceContext;
 
-  async componentDidMount() {
-    //fetch posts for workplace
-    const { wpId } = this.context;
-    await WpService.getWpPosts(wpId, "posts")
+  fetchPosts = (wpId, offset) => {
+    return WpService.getWpPosts(wpId, "posts", offset)
       .then(posts =>
         //set posts in context
         {
-          this.context.setPosts(posts);
+          if (posts.length < 10) {
+            this.setState({ disableLoadMore: true });
+          }
+          let currPosts = this.context.posts;
+          let allPosts = [...currPosts, ...posts];
+          this.context.setPosts(allPosts);
         }
       )
       .catch(err => this.setState({ err }));
+  };
+
+  async componentDidMount() {
+    //fetch posts for workplace
+    const { wpId } = this.context;
+    const { offset } = this.state;
+    await this.fetchPosts(wpId, offset);
   }
+
+  loadMorePosts = () => {
+    let offset = this.state.offset;
+    const { wpId } = this.context;
+    offset++;
+    this.fetchPosts(wpId, offset);
+    this.setState({ offset });
+  };
 
   render() {
     let posts = this.context.posts;
+    let { disableLoadMore } = this.state;
+    let show = this.props.className;
     return (
-      <div className="feed">
+      <div className={`${show} feed`}>
         <div className="post">
           <PostForm />
         </div>
         {posts ? posts.map((post, i) => <Post key={i} post={post} />) : <></>}
+        {disableLoadMore ? (
+          <></>
+        ) : (
+          <button className="load-more" onClick={this.loadMorePosts}>
+            load more
+          </button>
+        )}
       </div>
     );
   }
