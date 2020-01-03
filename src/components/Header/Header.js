b6cc099045a9b3514f1f9e19e4835917130469af
Miguel Ponce <miguelaponce258@gmail.com>
2020-01-02 23:51:02 -0700

    fix: clear context on sign out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,33 +10,11 @@ export default class Header extends Component {
   static contextType = WorkPlaceContext;
 
   handleLogoutClick = () => {
+    this.context.clearContext();
     TokenService.clearAuthToken();
     TokenService.clearCallbackBeforeExpiry();
     IdleService.unRegisterIdleResets();
   };
-
-  // renderLogoutLink(signOut) {
-  //   let location = this.props.location.pathname || null;
-  //   let { workPlace, nickname } = this.context;
-  //   let path = `workplace/${workPlace}/${nickname}`;
-  //   return (
-  //     <div className="header-logged-in">
-  //       {location === "/" ||
-  //       location === "/create" ||
-  //       location === "/join" ||
-  //       location === "/sign-in" ? (
-  //         <button
-  //           className="to-dash"
-  //           onClick={() => this.props.history.push(path)}
-  //         >
-  //           Back to WorkPlace
-  //         </button>
-  //       ) : (
-  //         <button onClick={() => this.signOut()}>Sign Out</button>
-  //       )}
-  //     </div>
-  //   );
-  // }
 
   renderLogoutLink(signOut) {
     let { workPlace, userId } = this.context;
@@ -52,18 +30,13 @@ export default class Header extends Component {
             Back to WorkPlace
           </button>
         ) : (
-          <Link className="login-out" onClick={signOut} to="/">
+          <Link className="login-out" onClick={this.handleLogoutClick} to="/">
             Sign out
           </Link>
         )}
       </div>
     );
   }
-
-  signOut = () => {
-    this.context.setLogged(false);
-    this.props.history.push("/");
-  };
 
   render() {
     return (
