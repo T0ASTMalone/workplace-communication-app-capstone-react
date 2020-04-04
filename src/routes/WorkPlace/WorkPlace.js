5227869a02b81e525b4ba15a8b4c013da164811b
Miguel Ponce <miguelaponce258@gmail.com>
2020-04-04 15:05:26 -0600

    fix: added labels to buttons

--- src/routes/WorkPlace/WorkPlace.js
+++ src/routes/WorkPlace/WorkPlace.js
@@ -11,7 +11,7 @@ import Footer from "../../components/Footer/Footer";
 
 class WorkPlace extends Component {
   state = {
-    main: "feed"
+    main: "feed",
   };
 
   static contextType = WorkPlaceContext;
@@ -19,7 +19,7 @@ class WorkPlace extends Component {
   async componentDidMount() {
     let { user, wp } = this.props.match.params;
     //get user info by id
-    await WpService.getUserInfo(user).then(async res => {
+    await WpService.getUserInfo(user).then(async (res) => {
       const { username, type, nickname, user_id, wp_id, code } = res;
       //set user info in context
       await this.context.setUserType(type);
@@ -35,7 +35,7 @@ class WorkPlace extends Component {
     //  display pending users component
   }
 
-  updateWpMain = e => {
+  updateWpMain = (e) => {
     // toggle between wp feed and ideas feed
     this.setState({ main: e });
   };
@@ -44,46 +44,48 @@ class WorkPlace extends Component {
     const { main, ready } = this.state;
     const { userName, nickname, userType, workPlace, wpCode } = this.context;
     return (
-      <div className="workplace">
-        <div className="workplace-header">
-          <div className="workplace-info card">
-            <div className="user-info">
-              <h2 className="user nickname">{nickname}</h2>
-              <h3 className="user user-name">{userName}</h3>
-              <h3 className="user user-type">
+      <div className='workplace'>
+        <div className='workplace-header'>
+          <div className='workplace-info card'>
+            <div className='user-info'>
+              <h2 className='user nickname'>{nickname}</h2>
+              <h3 className='user user-name'>{userName}</h3>
+              <h3 className='user user-type'>
                 {userType === "user" ? "member" : userType}
               </h3>
             </div>
-            <div className="wp-info">
-              <h2 className="workplace-name">{workPlace}</h2>
+            <div className='wp-info'>
+              <h2 className='workplace-name'>{workPlace}</h2>
               {userType === "creator" ? (
-                <p className="wpCode">wp code: {wpCode}</p>
+                <p className='wpCode'>wp code: {wpCode}</p>
               ) : (
                 <></>
               )}
             </div>
           </div>
 
-          <div className="new-members">
+          <div className='new-members'>
             {userType === "creator" ? <NewMembers /> : <></>}
           </div>
 
-          <div className="tabs ">
+          <div className='tabs '>
             <button
-              id="feed"
-              className="tab"
-              value="feed"
-              onClick={e => this.updateWpMain(e.target.value)}
+              id='feed'
+              className='tab'
+              aria-label='main-feed'
+              value='feed'
+              onClick={(e) => this.updateWpMain(e.target.value)}
             ></button>
             <button
-              className="tab"
-              id="ideas"
-              value="ideas"
-              onClick={e => this.updateWpMain(e.target.value)}
+              className='tab'
+              aria-label='ideas-feed'
+              id='ideas'
+              value='ideas'
+              onClick={(e) => this.updateWpMain(e.target.value)}
             ></button>
           </div>
         </div>
-        <div className="workplace-main">
+        <div className='workplace-main'>
           {ready ? (
             <>
               <Feed className={main === "feed" ? "feed" : "hidden"} />
@@ -102,7 +104,7 @@ class WorkPlace extends Component {
 WorkPlace.propTypes = {
   history: PropTypes.object,
   location: PropTypes.object,
-  match: PropTypes.object
+  match: PropTypes.object,
 };
 
 export default WorkPlace;
